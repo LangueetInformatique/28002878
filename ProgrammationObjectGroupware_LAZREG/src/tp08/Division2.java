e15553e547754478242118032cad7ff1bb7c63c7
LAZREG Mustapha <mustapha@MacBook-Air-de-LAZREG.local>
2020-11-23 19:19:24 +0100

    adding missing files

---
+++ ProgrammationObjectGroupware_LAZREG/src/tp08/Division2.java
@@ -0,0 +1,20 @@
+package tp08;
+
+public class Division2 {
+	
+	public static void main(String[] args) {
+		try {
+		division();
+		}catch(ArithmeticException e) {
+			System.out.println("Division par 0");
+		}
+	}
+	
+	public static void division() throws ArithmeticException{
+		int i = 1000, j;
+		do{
+			i--;
+			j=1/i;
+		}while(true);
+	}
+}
