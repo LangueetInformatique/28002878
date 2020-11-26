bdd8553e939fc98a6dcbdfca706df92ecd1d99b9
LAZREG Mustapha <mustapha@MacBook-Air-de-LAZREG.local>
2020-11-26 10:47:03 +0100

    update

---
+++ ProgrammationObjectGroupware_LAZREG/src/util/Keyboard.java
@@ -0,0 +1,16 @@
+package util;
+
+public class Keyboard {
+	
+	public Keyboard() {}
+
+	public void println(String s) {		
+		System.out.println(s);	
+	}
+
+	public static String getString(String string) {
+		return string;
+		
+	}	
+
+}
