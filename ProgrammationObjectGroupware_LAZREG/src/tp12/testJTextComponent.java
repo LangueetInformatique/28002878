e8796842085fa9ea5fec6e9657413210843eaf24
LAZREG Mustapha <mustapha@MacBook-Air-de-LAZREG.local>
2020-12-17 10:26:03 +0100

    test exo01

---
+++ ProgrammationObjectGroupware_LAZREG/src/tp12/testJTextComponent.java
@@ -0,0 +1,30 @@
+package tp12;
+
+/**
+ * 
+ * @source atelier12
+ *
+ */
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class testJTextComponent {
+
+
+	public static void main(String[] args) {
+		// Declaration de la JFrame
+		 JFrame fen = new JFrame("testJTextField"); fen.setSize(280, 280); 
+		 JPanel p = new JPanel();
+		 fen.getContentPane().add(p);
+		 JTextField jtf = new JTextField("Mustapha");
+		 p.add(jtf);
+		 
+		 
+//			Affichage de la JFrame et de son contenu
+		 fen.setVisible(true);
+
+	}
+
+}
